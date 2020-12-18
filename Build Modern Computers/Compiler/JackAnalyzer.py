fba35341a3e7c9b871ba7f0b5b7368b6700e1b0a
Ahmed Hatem <68568877+el-hatem@users.noreply.github.com>
2020-12-18 03:06:42 +0200

    Add files via upload

---
+++ Build Modern Computers/Compiler/JackAnalyzer.py
@@ -0,0 +1,38 @@
+import os
+import JackTokenizer as jt
+import CompilationEngineFinal as ce 
+import SymbolTable as ST
+import VMWriter
+import FuncTable
+import sys 
+
+input=sys.argv[1]
+fileList=[]
+if os.path.isdir(input):
+    if input.endswith('/'):
+        input=input[:-1]
+    os.chdir(input)
+    for file in os.listdir('.'):
+        if file.endswith('.jack'):
+            fileList.append(file)
+elif os.path.isfile(input):
+    fileList=[input]
+else:
+    raise Exception("Input should be either a file name or a directory name!")
+
+fntable = FuncTable.FuncTable()
+
+for file in fileList:
+    inpname = file.split('/')[-1].split('.')[0]
+    outFile = inpname + '.vm' 
+    tokenizer=jt.JackTokenizer(file)
+    table = ST.SymbolTable()
+    vm = VMWriter.VMWriter(outFile)
+    print "Compiling {0}".format(inpname)
+    compiler=ce.CompilationEngine(tokenizer, table, vm, inpname, fntable)
+    compiler.CompileClass()
+
+if not fntable.isemptyundec():
+    for (a, b) in fntable.undecfnlist:
+        raise Exception("Undefined function {0} in {1} class".format(b, a))
+
